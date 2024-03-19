0d1f8b150877ec044164b9929732460c8caa28d1
Ứng Hùng <111266351+unghung18@users.noreply.github.com>
2024-03-19 23:21:31 +0700

    fix bugs

--- src/app/(shop)/login/page.tsx
+++ src/app/(shop)/login/page.tsx
@@ -1,49 +1,9 @@
-"use client";
-
-import SegmentedControl from '@/components/SegmentedControl/SegmentedControl';
-import { useRef, useState, useEffect } from "react";
-
+import React from 'react';
 
 const page = () => {
-
-    const [selectedValue, setSelectedValue] = useState("ao");
-
-    useEffect(() => {
-        console.log(selectedValue);
-    }, [selectedValue]);
     return (
-        <div>
-            <SegmentedControl
-                name="group-2"
-                callback={(val: any) => setSelectedValue(val)}
-                controlRef={useRef()}
-                defaultIndex={0}
-                segments={[
-                    {
-                        label: "Áo",
-                        value: "ao",
-                        ref: useRef()
-                    },
-                    {
-                        label: "Quần",
-                        value: "quan",
-                        ref: useRef()
-                    },
-                    {
-                        label: "Chân váy",
-                        value: "chan vay",
-                        ref: useRef()
-                    }
-                    ,
-                    {
-                        label: "Đầm",
-                        value: "dam",
-                        ref: useRef()
-                    }
-                ]}
-            />
-        </div>
+        <div>page</div>
     )
 }
 
-export default page;
+export default page
