a45e663f24ef83e0d0e5915bd61842b0292c9daf
Ứng Hùng <111266351+unghung18@users.noreply.github.com>
2024-04-25 10:18:06 +0700

    update code

--- src/app/(shop)/product/page.tsx
+++ src/app/(shop)/product/page.tsx
@@ -1,12 +1,277 @@
-import React, { Suspense } from 'react';
-import ProductPage from './ProductPage';
+"use client"
 
-const page = () => {
+import React, { useState, useEffect } from 'react';
+import '../../../styles/product/product.scss';
+import { Slider } from 'antd';
+import { FaChevronDown } from "react-icons/fa6";
+import { categoryProductPage, colorData } from '../../../../contants';
+import ProductCard from '@/components/ProductCard';
+import { useRouter } from 'next/navigation';
+import { IoClose } from "react-icons/io5";
+import { ProductProps } from '@/types/types';
+import { getProducts } from '@/api/LanieApi';
+import SkeletonProductCard from '@/components/SkeletonProductCard/SkeletonProductCard';
+
+const Page = ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
+
+    const [selectedLayout, setSelectedLayout] = useState(3);
+    const [loading, setLoading] = useState(true);
+    const [products, setProducts] = useState<ProductProps[]>([]);
+
+    const router = useRouter();
+
+    const category = searchParams.category ? searchParams.category : "";
+    const size = searchParams.size ? searchParams.size : "";
+    const color = searchParams.color ? searchParams.color : "";
+    const sale = searchParams.sale ? searchParams.sale : "";
+    const search = searchParams.search ? searchParams.search : "";
+
+    let queryParams: any;
+
+    const handleCategory = (cate: string) => {
+        if (typeof window !== "undefined") {
+            queryParams = new URLSearchParams(window.location.search);
+        }
+
+        if (queryParams.has("category")) {
+            if (queryParams.get("category") == cate) {
+                queryParams.delete("category");
+            }
+            else {
+                queryParams.set("category", cate);
+            }
+        } else {
+            queryParams.append("category", cate);
+        }
+
+        const path = window.location.pathname + "?" + queryParams.toString();
+        router.push(path, { scroll: false });
+    }
+
+    const handleSize = (sz: string) => {
+        if (typeof window !== "undefined") {
+            queryParams = new URLSearchParams(window.location.search);
+        }
+
+        if (queryParams.has("size")) {
+            if (queryParams.get("size") == sz) {
+                queryParams.delete("size");
+            }
+            else {
+                queryParams.set("size", sz);
+            }
+        } else {
+            queryParams.append("size", sz);
+        }
+
+        const path = window.location.pathname + "?" + queryParams.toString();
+        router.push(path, { scroll: false });
+    }
+
+    const handleColor = (cl: string) => {
+        if (typeof window !== "undefined") {
+            queryParams = new URLSearchParams(window.location.search);
+        }
+
+        if (queryParams.has("color")) {
+            if (queryParams.get("color") == cl) {
+                queryParams.delete("color");
+            }
+            else {
+                queryParams.set("color", cl);
+            }
+        } else {
+            queryParams.append("color", cl);
+        }
+
+        const path = window.location.pathname + "?" + queryParams.toString();
+        router.push(path, { scroll: false });
+    }
+
+    const handleCheckSale = () => {
+
+        if (typeof window !== "undefined") {
+            queryParams = new URLSearchParams(window.location.search);
+        }
+
+        if (queryParams.has("sale")) {
+            queryParams.delete("sale");
+        } else {
+            queryParams.append("sale", "true");
+        }
+
+        const path = window.location.pathname + "?" + queryParams.toString();
+        router.push(path, { scroll: false });
+    }
+
+    const getAllProducts = async () => {
+        setLoading(true);
+        try {
+            if (typeof window !== "undefined") {
+                queryParams = new URLSearchParams(window.location.search);
+            }
+
+            const res = await getProducts(queryParams.toString())
+            console.log(res.data);
+            if (res.error) {
+                alert(res.error.message)
+            }
+
+            else {
+                setProducts(res.data)
+            }
+
+            setLoading(false);
+        } catch (error) {
+            alert(error)
+        }
+    }
+
+    useEffect(() => {
+        getAllProducts();
+    }, [category, sale, color, size, search]);
+
     return (
-        <Suspense>
-            <ProductPage />
-        </Suspense>
+        <div className='product'>
+            <div className='breadcum'>
+                <div className='breadcum__container container'>
+                    <span>TRANG CHỦ</span>
+                    <span>/</span>
+                    <span>SẢN PHẨM</span>
+                </div>
+            </div>
+            <img src="/banner-product.webp" alt="outfit of the day" />
+
+            <div className='product__container container'>
+                <div className='product-sidebar'>
+                    <div className="filter-type">
+                        <div className="filter-heading">Product Type</div>
+                        <ul className='list-type'>
+                            {categoryProductPage.map((item) => (
+                                <li className={`list-type__item ${category == item.name.toLocaleLowerCase() ? "active" : ""}`} key={item._id} onClick={() => handleCategory(item.name.toLocaleLowerCase())}>
+                                    <span>{item.name}</span>
+                                    <span>({item.quantity})</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="filter-size">
+                        <div className="filter-heading">Size</div>
+                        <ul className='list-size'>
+                            <li className={`list-size__item ${size == "s" ? "active" : ""}`} onClick={() => handleSize('s')}>S</li>
+                            <li className={`list-size__item ${size == "m" ? "active" : ""}`} onClick={() => handleSize('m')}>M</li>
+                            <li className={`list-size__item ${size == "l" ? "active" : ""}`} onClick={() => handleSize('l')}>L</li>
+                            <li className={`list-size__item ${size == "xl" ? "active" : ""}`} onClick={() => handleSize('xl')}>XL</li>
+                            <li className={`list-size__item ${size == "2xl" ? "active" : ""}`} onClick={() => handleSize('2xl')}>2XL</li>
+                            <li className={`list-size__item ${size == "3xl" ? "active" : ""}`} onClick={() => handleSize('3xl')}>3XL</li>
+                        </ul>
+                    </div>
+                    <div className="filter-price">
+                        <div className="filter-heading">Price</div>
+                        <div>
+                            <Slider range defaultValue={[0, 50]} />
+                        </div>
+                    </div>
+                    <div className="filter-color">
+                        <div className="filter-heading">Colors</div>
+                        <ul className='list-color'>
+                            {colorData.map((item: any) => (
+                                <li className={`list-color__item ${color == item._id ? "active" : ""}`} key={item._id} onClick={() => handleColor(item._id.toLocaleLowerCase())}>
+                                    <span style={{ backgroundColor: `${item.color}` }}></span>
+                                    <span>{item.name}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    {/*  <div className="filter-brand">
+                        <div className="filter-heading">Brands</div>
+                    </div> */}
+                </div>
+                <div className='product-list'>
+                    <div className='product-list__heading'>
+                        <div className='left' style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+                            <div className='choose-layout' >
+                                <div className={`choose-layout__three ${selectedLayout == 3 && "active"}`} onClick={() => setSelectedLayout(3)}>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                                <div className={`choose-layout__four ${selectedLayout == 4 && "active"}`} onClick={() => setSelectedLayout(4)}>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </div>
+                            <div className='check-sale'>
+                                <input type="checkbox" name="filtersale" id='filtersale' style={{ display: "none" }} checked={sale ? true : false} onChange={handleCheckSale} />
+                                <label htmlFor="filtersale">Show only products on sale</label>
+                            </div>
+                        </div>
+                        <div className='right'>
+                            <div className='select-block'>
+                                <select name="select-filter" id="select-filter" defaultValue="all">
+                                    <option value="all">Tất cả</option>
+                                    <option value="soldQuantityHighToLow">Bán chạy nhất</option>
+                                    <option value="discountHighToLow">Sản phẩm mới nhất</option>
+                                    <option value="priceHighToLow">Giá từ cao đến thấp</option>
+                                    <option value="priceLowToHigh">Giá từ thấp đến cao</option>
+                                </select>
+                                <div className='select-arrow'>
+                                    <FaChevronDown size={12} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='product-list__filters'>
+                        <div className='total-product'>40 <span style={{ color: "rgb(105 108 112)" }}>kết quả tìm kiếm</span></div>
+                        <div className='line'></div>
+                        <div className='list__filters'>
+                            {category &&
+                                <div className='item' onClick={() => handleCategory(category)}>
+                                    <IoClose size={18} />
+                                    <span>{category.charAt(0).toUpperCase() + category.slice(1)}</span>
+                                </div>}
+                            {size &&
+                                <div className='item' onClick={() => handleSize(size)}>
+                                    <IoClose size={18} />
+                                    <span>{size.toLocaleUpperCase()}</span>
+                                </div>}
+                            {color &&
+                                <div className='item' onClick={() => handleColor(color)}>
+                                    <IoClose size={18} />
+                                    <span>{color}</span>
+                                </div>}
+                        </div>
+                        {
+                            category || color || size ?
+                                <div className='clear-button' onClick={() => router.push("/product", { scroll: false })}>
+                                    <IoClose size={18} />
+                                    <span>Reset</span>
+                                </div>
+                                :
+                                <div></div>
+                        }
+                    </div>
+                    <div className='product-list__main' style={{ gridTemplateColumns: `repeat(${selectedLayout}, 1fr)` }}>
+                        {
+                            loading ?
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item, index) => (
+                                    <SkeletonProductCard key={index} />
+                                ))
+                                :
+                                products.length > 0 ? products.map((item) => (
+                                    <ProductCard product={item} key={item._id} />
+                                ))
+                                    :
+                                    <div> Không tìm thấy sản phẩm nào</div>
+                        }
+
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
-export default page
+export default Page
--- src/app/checkout/[id]/page.tsx
+++ src/app/checkout/[id]/page.tsx
@@ -1,51 +1,205 @@
-import React from 'react';
-import '../../../styles/checkout/checkout.scss';
-import { MdOutlineChevronRight } from "react-icons/md";
+'use client';
 
-const Page = ({ params }: {
-    params: {
-        id: string;
+import { useAppSelector } from '@/redux/hooks';
+// import PaypalCheckoutBtn from '@/components/PaypalCheckoutBtn';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { ToastContainer, toast, Slide } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+const Page = () => {
+
+    const [divisons, setDivisions] = useState<any>([]);
+    const [districts, setDistricts] = useState<any>([]);
+    const [wards, setWards] = useState<any>([]);
+
+    const [info, setInfo] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        address: ""
+    });
+    /* const [name, setName] = useState();
+    const [name, setName] = useState(); */
+
+    /* const [selectedCity, setSelectedCity] = useState();
+    const [selectedDistrict, setSelectedDistrict] = useState();
+    const [selectedWard, setSelectedWard] = useState(); */
+
+    const [deliveryMethod, setDeliveryMethod] = useState("1");
+    const [paymentMethod, setPaymentMethod] = useState("1");
+    const [totalPayment, setTotalPayment] = useState<number>(0);
+
+    const { totalAmount } = useAppSelector(state => state.cart);
+    const router = useRouter();
+
+    const getAllDivisions = async () => {
+        try {
+            const res = await axios.get("https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json")
+            setDivisions(res.data)
+        } catch (error) {
+            console.log(error)
+        }
     }
-}) => {
-    return (
-        <div className='checkout-page'>
-            <div className='checkout-page__container'>
-                <div className='checkout-sidebar'>
-                    <h1 className='logo'>LANIE</h1>
-                    <div className='breadscum'>
-                        <div>Giỏ hàng</div>
-                        <span>
-                            <MdOutlineChevronRight />
-                        </span>
-                        <div>Thông tin vận chuyển</div>
-                        <span>
-                            <MdOutlineChevronRight />
-                        </span>
-                        <div>Phương thức thanh toán</div>
-                    </div>
-                    <div className='title'>Thông tin thanh toán</div>
 
-                    <div className='form-order'>
-                        <div>
-                            <label htmlFor="name">Họ và tên</label>
-                            <input type="text" placeholder='Họ và tên' id='name' />
-                        </div>
-                        <div>
-                            <div>
-                                <label htmlFor="name">Họ và tên</label>
-                                <input type="text" placeholder='Họ và tên' id='name' />
+    const onChangeDelivaryMethod = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDeliveryMethod(event.target.value);
+    }
+    const onChangePaymentMethod = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPaymentMethod(event.target.value);
+    }
+
+    const handleChangeProvince = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+        if (event.target.value !== "default") {
+            const result = divisons.filter((item: any) => item.Id == event.target.value);
+            setDistricts(result[0].Districts);
+        }
+    }
+    const handleChangeDistrict = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+        if (event.target.value !== "default") {
+            const result = districts.filter((item: any) => item.Id == event.target.value);
+            setWards(result[0].Wards);
+        }
+    }
+
+    const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInfo({ ...info, [event.target.name]: event.target.value })
+    }
+
+    const handleOrder = () => {
+        console.log(info)
+        if (info.name == '' || info.email == '' || info.phone == '' || info.address == '') {
+            toast.error("Vui lòng nhập đầy đủ thông tin", {
+                theme: "colored"
+            })
+        }
+        else {
+            router.push('/checkout/success')
+        }
+    }
+
+    useEffect(() => {
+        getAllDivisions();
+    }, []);
+
+    useEffect(() => {
+        setTotalPayment(totalAmount)
+    }, [totalAmount]);
+
+    return (
+        <>
+            <div className='px-5 md:px-10 lg:px-40 max-w-[1280px] mx-auto grid md:grid-cols-10 gap-5 py-10 bg-gradient-to-r from-[#faf8f1] to-[#f6f3ef] grid-cols-1 min-h-[100vh]'>
+                <div className=' space-y-3 col-span-7 bg-white py-4 px-5'>
+                    <h2>Thông tin thanh toán</h2>
+                    <input onChange={handleChangeInput} name='name' type="text" placeholder='Họ và tên' className='w-full placeholder:font-extralight placeholder:text-gray-500 outline-none border-2 p-2 rounded-md border-gray-400 text-[#333333] shadow-sm focus:border-[#338dbc] focus:border-2' />
+                    <div className='grid grid-cols-10'>
+                        <input onChange={handleChangeInput} name='email' type="email" placeholder='Email' className='col-span-7 placeholder:font-extralight placeholder:text-gray-500 outline-none border-2 p-2 rounded-md border-gray-400 mr-3 text-[#333333] shadow-sm focus:border-[#338dbc] focus:border-2' />
+                        <input onChange={handleChangeInput} name='phone' type="text" placeholder='Điện thoại' className='col-span-3 placeholder:font-extralight placeholder:text-gray-500 outline-none border-2 p-2 rounded-md border-gray-400 text-[#333333] shadow-sm focus:border-[#338dbc] focus:border-2' />
+                    </div>
+                    <input onChange={handleChangeInput} name='address' type="text" placeholder='Địa chỉ' className='w-full placeholder:font-extralight placeholder:text-gray-500 outline-none border-2 p-2 rounded-md border-gray-400 text-[#333333] shadow-sm focus:border-[#338dbc] focus:border-2' />
+                    <div className='grid grid-cols-3 gap-5'>
+                        <div className='p-2 border rounded-md border-gray-400 flex items-center'>
+                            <div className='flex flex-col w-full'>
+                                <label htmlFor='city' className='text-gray-500 font-thin'>Tỉnh</label>
+                                <select id="city" className='outline-none' onChange={handleChangeProvince}>
+                                    <option value="default">Chọn tỉnh thành</option>
+                                    {divisons && divisons.map((e: any) => (
+                                        <option value={e.Id} key={e.Id}>{e.Name}</option>
+                                    ))}
+                                </select>
                             </div>
-                            <div>
-                                <label htmlFor="name">Họ và tên</label>
-                                <input type="text" placeholder='Họ và tên' id='name' />
+                        </div>
+                        <div className='p-2 border rounded-md border-gray-400 flex items-center'>
+                            <div className='flex flex-col w-full'>
+                                <label htmlFor='district' className='text-gray-500 font-thin'>Quận/Huyện</label>
+                                <select id="district" className='outline-none' onChange={handleChangeDistrict}>
+                                    <option value="">Chọn quận huyện</option>
+                                    {districts && districts.map((e: any) => (
+                                        <option value={e.Id} key={e.Id}>{e.Name}</option>
+                                    ))}
+                                </select>
+                            </div>
+
+                        </div>
+                        <div className='p-2 border rounded-md border-gray-400 flex items-center'>
+                            <div className='flex flex-col w-full'>
+                                <label htmlFor='ward' className='text-gray-500 font-thin'>Phường/Xã</label>
+                                <select id="ward" className='outline-none'>
+                                    <option value="default">Chọn phường xã</option>
+                                    {wards && wards.map((e: any) => (
+                                        <option value={e.Id} key={e.Id}>{e.Name}</option>
+                                    ))}
+                                </select>
                             </div>
                         </div>
-                        <div></div>
+                    </div>
+                    <div className='grid grid-cols-1 gap-5'>
+                        <h3>Chọn phương thức giao hàng</h3>
+                        <div className='bg-[rgb(240,248,255)] p-5 border border-[#99CCFF] rounded-md'>
+                            <div className=''>
+                                <input onChange={onChangeDelivaryMethod} id='delivery1' type="radio" value="1" name="delivery" checked={deliveryMethod === "1"} />
+                                <label htmlFor="delivery1"><strong className=' text-yellow-500 ml-3'>FAST</strong> Giao hàng tiết kiệm</label><br />
+                                <input onChange={onChangeDelivaryMethod} id='delivery2' type="radio" value="2" name="delivery" checked={deliveryMethod === "2"} />
+                                <label htmlFor="delivery2"><strong className=' text-yellow-500 ml-3'>GO_JEK</strong> Giao hàng nhanh chóng</label><br />
+                            </div>
+                        </div>
+                        <h3>Chọn phương thức thanh toán</h3>
+                        <div className='bg-[#F0F8FF] p-5 border border-[#99CCFF] rounded-md'>
+                            <div className=''>
+                                <input onChange={onChangePaymentMethod} id='payment1' type="radio" value="1" name="payment" checked={paymentMethod === "1"} />
+                                <label className='ml-3' htmlFor="payment1">Thanh toán tiền mặt khi nhận hàng</label><br />
+
+                                <input onChange={onChangePaymentMethod} id='payment2' type="radio" value="2" name="payment" checked={paymentMethod === "2"} />
+                                <label className='ml-3' htmlFor="payment2">Thanh toán khi nhận thẻ tín dụng</label><br />
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className='checkout-sumary'>a</div>
+                <div className='col-span-3 max-h-[400px] flex flex-col gap-3'>
+                    <div className='rounded-lg p-5 bg-white'>
+                        <div className=' border-b-[#fffff] border-b-2 p-1 mb-4'>Địa chỉ: {info.address}</div>
+                        <div>
+                            <div className='flex justify-between items-center'>
+                                <p>Tạm tính</p>
+                                <strong>{totalPayment?.toLocaleString()} VND</strong>
+                            </div>
+                            <div className='flex justify-between items-center'>
+                                <p>Giảm giá</p>
+                                <strong>0 VND</strong>
+                            </div>
+                            <div className='flex justify-between items-center'>
+                                <p>Phí giao hàng</p>
+                                <strong>0 VND</strong>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Tổng tiền</p>
+                            <div className='text-center text-red-500'>
+                                <strong>{totalPayment?.toLocaleString()} VND</strong>
+                            </div>
+                        </div>
+                    </div>
+                    {
+                        paymentMethod === '1' ?
+                            <div className='flex-center py-3 px-2 bg-red-500 cursor-pointer rounded-md text-white' onClick={handleOrder}>
+                                Đặt hàng
+                            </div>
+                            :
+                            <button>Checkout</button>
+                        // <PaypalCheckoutBtn totalPayment={totalPayment} />
+                    }
+                </div>
             </div>
-        </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={1500}
+                limit={5}
+                transition={Slide}
+            />
+        </>
     )
 }
 
